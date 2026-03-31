d8e45632c64f34dee1c9a960e995f306c9209776
Krishgupta2006 <krrishgupta1008@gmail.com>
2026-03-31 22:53:05 +0530

    code

---
+++ day59.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Tree Node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create new node
+struct Node* newNode(int val) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+
+// Search in inorder
+int search(int inorder[], int start, int end, int value) {
+    for (int i = start; i <= end; i++) {
+        if (inorder[i] == value)
+            return i;
+    }
+    return -1;
+}
+
+// Build Tree
+struct Node* buildTree(int inorder[], int postorder[], int start, int end, int* postIndex) {
+    if (start > end)
+        return NULL;
+
+    // Last element of postorder = root
+    struct Node* root = newNode(postorder[*postIndex]);
+    (*postIndex)--;
+
+    // Leaf node
+    if (start == end)
+        return root;
+
+    // Find root in inorder
+    int inIndex = search(inorder, start, end, root->data);
+
+    // IMPORTANT: build right first
+    root->right = buildTree(inorder, postorder, inIndex + 1, end, postIndex);
+    root->left = buildTree(inorder, postorder, start, inIndex - 1, postIndex);
+
+    return root;
+}
+
+// Preorder traversal
+void preorder(struct Node* root) {
+    if (root == NULL)
+        return;
+
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+// -------- MAIN --------
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int inorder[n], postorder[n];
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &inorder[i]);
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &postorder[i]);
+
+    int postIndex = n - 1;
+
+    struct Node* root = buildTree(inorder, postorder, 0, n - 1, &postIndex);
+
+    preorder(root);
+
+    return 0;
+}
