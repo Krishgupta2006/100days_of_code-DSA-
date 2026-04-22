39cff935198aa2b9ee7e68f523fcc74db709c6e9
Krishgupta2006 <krrishgupta1008@gmail.com>
2026-04-22 19:08:43 +0530

    code

--- day80.c
+++ day80.c
@@ -1,41 +1,40 @@
 #include <stdio.h>
-#define INF 99999
-#define MAX 100
+
+#define INF 1000000000
 
 int main() {
     int n;
-    int dist[MAX][MAX];
-
     scanf("%d", &n);
 
-    // Input matrix
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    int dist[n][n];
+
+    // Input
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
             scanf("%d", &dist[i][j]);
 
-            if (dist[i][j] == -1 && i != j)
+            if(dist[i][j] == -1 && i != j)
                 dist[i][j] = INF;
         }
     }
 
     // Floyd-Warshall Algorithm
-    for (int k = 0; k < n; k++) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-
-                if (dist[i][k] + dist[k][j] < dist[i][j]) {
-                    dist[i][j] = dist[i][k] + dist[k][j];
+    for(int k = 0; k < n; k++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(dist[i][k] != INF && dist[k][j] != INF) {
+                    if(dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
                 }
-
             }
         }
     }
 
-    // Output result
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-
-            if (dist[i][j] == INF)
+    // Output
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(dist[i][j] == INF)
                 printf("-1 ");
             else
                 printf("%d ", dist[i][j]);
